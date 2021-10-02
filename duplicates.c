07d1c3457a7866556124428e4de12723f969fc7b
bird <20701908+bir-d@users.noreply.github.com>
2021-10-02 20:01:20 +0800

    Create duplicates.c

---
+++ duplicates.c
@@ -0,0 +1,7 @@
+//  CITS2002 Project 2 2021
+//  Name(s):             Cormac Sharkey, Avery Warddhana
+//  Student number(s):   22983427, 22984998
+
+//  Compile with: -std=c11 -Wall -Werror -pedantic
+//  Project must be compiled with `make`
+
